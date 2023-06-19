f73b97e70471c96150c6361da669df0c7c625725
davidjia223 <45841175+davidjia223@users.noreply.github.com>
2023-06-19 00:16:57 -0700

    callApi + new custome menu

--- appscript.js
+++ appscript.js
@@ -2,15 +2,32 @@
 const API_KEY = "sk-xxxx";
 const MODEL_TYPE = "gpt-3.5-turbo"; //chatGPT model
 
+
 // Creates a custom menu in Google Sheet
 function onOpen() {
   SpreadsheetApp.getUi().createMenu("ChatGPT")
       .addItem("Generate Keywords", "generateKeywords")
       .addItem("Create Ad Copy", "generateAdCopy")
+      .addItem("Search web related result", "generateWeb")
       .addToUi();
 }
 
+function generateWeb() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const selectedText = sheet.getActiveRange().getValue();
+  const prompt = "Generate the top 5 recommended websites for this keyword : " + selectedText;
+  const temperature = 0;
+  const maxTokens = 2050;
 
+  const requestBody = {
+    model: MODEL_TYPE,
+    messages: [{role: "user", content: prompt}],
+    temperature,
+    max_tokens: maxTokens,
+  };
+
+  callApi(requestBody, sheet); // Call the API function
+}
 
 function generateAdCopy() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
@@ -26,23 +43,7 @@ function generateAdCopy() {
     max_tokens: maxTokens,
   };
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + API_KEY,
-    },
-    payload: JSON.stringify(requestBody),
-  };
-
-
-  const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
-  const responseText = response.getContentText();
-  const json = JSON.parse(responseText);
-  const generatedText = json['choices'][0]['message']['content'];
-  Logger.log(generatedText);
-
-  sheet.getRange(sheet.getLastRow() + 1, 1).setValue(generatedText.toString());
+  callApi(requestBody, sheet); // Call the API function
 }
 
 
@@ -60,6 +61,11 @@ function generateKeywords() {
     max_tokens: maxTokens,
   };
 
+  callApi(requestBody, sheet); // Call the API function
+}
+
+// New function to handle API requests
+function callApi(requestBody, sheet) {
   const requestOptions = {
     method: "POST",
     headers: {
@@ -68,7 +74,6 @@ function generateKeywords() {
     },
     payload: JSON.stringify(requestBody),
   };
-
 
   const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
   const responseText = response.getContentText();
