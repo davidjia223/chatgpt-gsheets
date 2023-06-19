ce12007ac8118a7379103ea97310c65e8398fbd0
davidjia223 <45841175+davidjia223@users.noreply.github.com>
2023-06-19 12:29:17 -0700

    Updated createRequestBody
    When executed is done script.google it may give an error message indicating 'cannot call...'. This is normal, simply return to google sheets document and hit refresh. The new menu should be there as usual.

--- appscript.js
+++ appscript.js
@@ -2,24 +2,21 @@
 const API_KEY = "sk-xxxx";
 const MODEL_TYPE = "gpt-3.5-turbo"; //chatGPT model
 
-
 // Creates a custom menu in Google Sheet
 function onOpen() {
-  SpreadsheetApp.getUi().createMenu("ChatGPT")
-      .addItem("Generate Keywords", "generateKeywords")
-      .addItem("Create Ad Copy", "generateAdCopy")
-      .addItem("Search web related result", "generateWeb")
-      .addToUi();
+  SpreadsheetApp.getUi()
+    .createMenu("ChatGPT")
+    .addItem("Generate Keywords", "generateKeywords")
+    .addItem("Create Ad Copy", "generateAdCopy")
+    .addItem("Search web related result", "generateWeb")
+    .addToUi();
 }
 
 function generateWeb() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   const selectedText = sheet.getActiveRange().getValue();
-  const prompt = "Generate the top 5 recommended websites for this keyword : " + selectedText;
-  const temperature = 0;
-  const maxTokens = 2050;
-
-  const requestBody = createRequestBody(prompt, temperature, maxTokens);
+  const prompt = "Generate the top 5 recommended websites for this keyword: " + selectedText;
+  const requestBody = createRequestBody(prompt);
 
   callApi(requestBody, sheet); // Call the API function
 }
@@ -27,26 +24,32 @@ function generateWeb() {
 function generateAdCopy() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   const selectedText = sheet.getActiveRange().getValue();
-  const prompt = "Generate 5 Google Adwords Copies for this keyword : " + selectedText;
-  const temperature = 0;
-  const maxTokens = 2050;
-
-  const requestBody = createRequestBody(prompt, temperature, maxTokens);
+  const prompt = "Generate 5 Google Adwords Copies for this keyword: " + selectedText;
+  const requestBody = createRequestBody(prompt);
 
   callApi(requestBody, sheet); // Call the API function
 }
 
-
 function generateKeywords() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   const selectedText = sheet.getActiveRange().getValue();
-  const prompt = "Generate 10 Keywords similar to this keyword : " + selectedText;
+  const prompt = "Generate 10 Keywords similar to this keyword: " + selectedText;
+  const requestBody = createRequestBody(prompt);
+
+  callApi(requestBody, sheet); // Call the API function
+}
+
+// Create the requestBody object
+function createRequestBody(prompt) {
   const temperature = 0;
   const maxTokens = 2050;
 
-  const requestBody = createRequestBody(prompt, temperature, maxTokens);
-
-  callApi(requestBody, sheet); // Call the API function
+  return {
+    model: MODEL_TYPE,
+    messages: [{ role: "user", content: prompt }],
+    temperature,
+    max_tokens: maxTokens,
+  };
 }
 
 // New function to handle API requests
@@ -60,20 +63,13 @@ function callApi(requestBody, sheet) {
     payload: JSON.stringify(requestBody),
   };
 
-  // Function to create the requestBody object
-function createRequestBody(prompt, temperature, maxTokens) {
-  return {
-    model: MODEL_TYPE,
-    messages: [{ role: "user", content: prompt }],
-    temperature,
-    max_tokens: maxTokens,
-  };
-}
-
-  const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+  const response = UrlFetchApp.fetch(
+    "https://api.openai.com/v1/chat/completions",
+    requestOptions
+  );
   const responseText = response.getContentText();
   const json = JSON.parse(responseText);
-  const generatedText = json['choices'][0]['message']['content'];
+  const generatedText = json["choices"][0]["message"]["content"];
   Logger.log(generatedText);
 
   sheet.getRange(sheet.getLastRow() + 1, 1).setValue(generatedText.toString());
